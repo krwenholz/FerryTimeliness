a6baa57c22f677350234dc1a387ff63587670f73
Kyle R. Wenholz <kyle@krwenholz.com>
2012-10-24 11:35:33 -0700

    Now I can count what data is bad and what is good

--- SeniorProject/source/data_request_input.py
+++ SeniorProject/source/data_request_input.py
@@ -20,8 +20,6 @@ def write_to_database(datas):
     """
         Takes in a data reader for the csv file and puts it all in a table.
     """
-    # TODO: I need to just pull out the time instead of date (or else it relies
-    #   on date past 2000 rather than time of day.
     FMT = '%m/%d/%Y %H:%M'
     FMT_DATE = '%m/%d/%Y'
     
@@ -35,17 +33,35 @@ def write_to_database(datas):
         #return
     #cur = conn.cursor()
     query_string = "INSERT INTO (vessel, departing, arriving, scheduled_departure, actual_departure, route_name, initial_eta, actual_arrival, date) VALUES(%s, %s, %s, %d, %d, %s, %d, %d, %s);"
+    bad_time = 0
+    bad_datatype = 0
+    good = 0
     for row in datas:
         # times are in (3, 4, 6, 7)
         try:
             if (datetime.strptime(row[4], FMT) - datetime.strptime(row[7], FMT)).total_seconds() > 0:
                 # departure - arrival > 0 checks for a proper time entry
-                print 'Found bad data with ', row
+                #print 'Found bad data with ', row
+                bad_time = bad_time + 1
+            elif:
+                any([(rr == 'NULL') for rr in row])
+                bad_datatype = bad_datatype + 1
             else:
-                print datetime.strptime(reader.next()[8], FMT_DATE).date()
-                query_data = (
-                cur.execute(query_string, query_data)
-#
+                good = good + 1
+                #query_data = (row[0], row[1], row[2],
+                #              make_time(row[3].split(' ')),
+                #              make_time(row[4].split(' ')),
+                #              row[5],
+                #              make_time(row[6].split(' ')),
+                #              make_time(row[7].split(' ')),
+                #              datetime.strptime(row[8], FMT_DATE).date())
+                #cur.execute(query_string, query_data)
+        except:
+            #print 'Found bad data with ', row
+            bad1 = bad1 + 1
+    print 'bad timings = ', bad_time
+    print 'bad data type = ', bad_datatype
+    print 'good data = ', good
 #    conn.commit()
 #    cur.close()
 #    conn.close()
