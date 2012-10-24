fa24739a7fea916563e372713745b418c1b4a1e8
Kyle R. Wenholz <kyle@krwenholz.com>
2012-10-23 19:12:36 -0700

    Added in most of the checks for good data.  Preparing to insert data!  Oh and there's now a method to make a time into a straight up int.

--- SeniorProject/source/data_request_input.py
+++ SeniorProject/source/data_request_input.py
@@ -9,32 +9,46 @@ import psycopg2
     PostgreSQL database.
 """
 
+def make_time(daytime):
+    """
+        Use just the time of day to make an integer value for the time.
+    """
+    return time.mktime(time.strptime('1970-01-01 '+daytime, 
+                                     '%Y-%m-%d %H:%M'))-28800
+
 def write_to_database(datas):
     """
-        Takes in the formatted html data for ferries and writes it to the 
-        database sitting in 409.
+        Takes in a data reader for the csv file and puts it all in a table.
     """
+    # TODO: I need to just pull out the time instead of date (or else it relies
+    #   on date past 2000 rather than time of day.
+    FMT = '%m/%d/%Y %H:%M'
+    FMT_DATE = '%m/%d/%Y'
+    
+    print 'Preparing to write data for ', datas.next()
     conn = None
     try:
         conn = psycopg2.connect("dbname='ferry_data' user='ferry_user' host='localhost' password='development2012'");
     except Exception as e:
         print e
         print "I am unable to connect to the database"
-        return
-    cur = conn.cursor()
-    for dd in datas:
-        # TODO: adjust the insert to work with another column
-        query_string = "INSERT INTO (vessel, knots, departing, arriving, scheduled_departure, actual_departure, estimated_arrival, route, date, flags) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, 'now', %s);"
-        query_data = (dd['vessel'], dd['knots'], dd['departing'],
-                                   dd['arriving'], dd['scheduled_departure'], 
-                                   dd['actual_departure'], dd['est_arrival'],
-                                   dd['route'], dd['flags'],)
-        print query_string % query_data
-        cur.execute(query_string, query_data)
-
-    conn.commit()
-    cur.close()
-    conn.close()
+        #return
+    #cur = conn.cursor()
+    query_string = "INSERT INTO (vessel, departing, arriving, scheduled_departure, actual_departure, route_name, initial_eta, actual_arrival, date) VALUES(%s, %s, %s, %d, %d, %s, %d, %d, %s);"
+    for row in datas:
+        # times are in (3, 4, 6, 7)
+        try:
+            if (datetime.strptime(row[4], FMT) - datetime.strptime(row[7], FMT)).total_seconds() > 0:
+                # departure - arrival > 0 checks for a proper time entry
+                print 'Found bad data with ', row
+            else:
+                print datetime.strptime(reader.next()[8], FMT_DATE).date()
+                query_data = (
+                cur.execute(query_string, query_data)
+#
+#    conn.commit()
+#    cur.close()
+#    conn.close()
 
 
 ##########
@@ -42,15 +56,6 @@ def write_to_database(datas):
 ##########
 fname = '/home/krwenholz/Dropbox/Senior/ThesisStorage/data_request_October2012.csv'
 reader = csv.reader(open(fname, 'rb'))
-print reader.next()[3]
-FMT = '%m-%d-%Y %H:%M'
-y2k = datetime.strptime('1-1-2000 00:00', FMT)
-t1 = datetime.strptime(reader.next()[3].replace('/','-'), FMT) 
-t2 = datetime.strptime(reader.next()[3].replace('/','-'), FMT)
-print (t1 - y2k).total_seconds()
-
-
-
-#write_to_database(reader)
+write_to_database(reader)
 
 
