e771cdf3b3a5ec953d30905a00d2224ee5716b6a
Kyle R. Wenholz <kyle@krwenholz.com>
2012-10-30 18:17:00 -0700

    Determining and vectorizing the categorical variables now seems to work nicely.

--- source/libsvm_data_format.py
+++ source/libsvm_data_format.py
@@ -8,11 +8,26 @@ def is_number(s):
     except ValueError:
         return False
 
-# TODO: check the categorical variables to see what categories I need.
-def categoricals(list1):
-    return [ii for ii in range(len(list1)) if not is_number(list1[ii])]
+def categoricals(datas):
+    """
+        Finds indices in a list which are not numbers.
+    """
+    return [ii for ii in range(len(datas)) if not is_number(datas[ii])]
 
 # TODO: turn categorical variables into things like feature vectors
+def make_features(indices, datas):
+    feature_sets = [list(set([row[ii] for row in datas])) for ii in indices]
+    print 'Found in columns ', indices, ' the following values.\n', feature_sets
+    print 'These columns will be replaced with an appended feature vector.'
+    for row in datas:
+        features = [row[ii] for ii in indices]
+        for ii in indices:
+            row.pop(ii)
+        row.extend([0 if not attr==features[ii] else 1 
+                      for ii in range(len(features))
+                      for attr in feature_sets[ii]])
+    return datas
+
 # TODO: scale the data in my highest*1.5 method
 # TODO: make sure to output anything done specially in the above (e.g. I have
 #   x ferries and each one corresponds to a '1' in position y of the final 
@@ -27,8 +42,10 @@ weather_read = csv.reader(open(sys.argv[1], 'rb'))
 departure_read = csv.reader(open(sys.argv[2], 'rb'))
 weather_list = [row for row in weather_read]
 departure_list = [row for row in departure_read]
-print weather_list.pop(0)
-print departure_list.pop(0)
-print categoricals(weather_list[1])
-print categoricals(departure_list[1])
+print 'Weather is ', weather_list.pop(0)
+print 'Departure list is ', departure_list.pop(0)
+# I have to remove the date from the end for departures and date from start for 
+# weather
+make_features(categoricals(departure_list[0])[:-1], departure_list)
+print departure_list[0]
 # arrival_read = csv.reader(open(sys.argv[3], 'rb'))
