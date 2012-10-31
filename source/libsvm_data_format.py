d465d19fd4bd20a3808dc5dde3968b068a5d80ba
Kyle R. Wenholz <kyle@krwenholz.com>
2012-10-30 21:20:23 -0700

    The data is almost joined (next scaling), but a comparison for dates isn't working.  This is marked.  Sky code was removed from weather since it created an insane number of attributes.

--- source/libsvm_data_format.py
+++ source/libsvm_data_format.py
@@ -2,6 +2,9 @@ import csv
 import sys
 
 def is_number(s):
+    """
+        Is s a number?  True is 'tis so.  False otherwise.
+    """
     try:
         float(s)
         return True
@@ -14,21 +17,77 @@ def categoricals(datas):
     """
     return [ii for ii in range(len(datas)) if not is_number(datas[ii])]
 
-# TODO: turn categorical variables into things like feature vectors
 def make_features(indices, datas):
+    """
+        Given an iterable of indices, for every row in datas, we remove the
+        offending index and append a bit-vector equivalent to the end of the
+        row. This modifies datas.
+    """
+    indices.sort()
     feature_sets = [list(set([row[ii] for row in datas])) for ii in indices]
     print 'Found in columns ', indices, ' the following values.\n', feature_sets
-    print 'These columns will be replaced with an appended feature vector.'
+    print 'These columns will be replaced with an appended feature vector.\n'
     for row in datas:
         features = [row[ii] for ii in indices]
-        for ii in indices:
-            row.pop(ii)
+        for ii in range(len(indices)):
+            row.pop(indices[ii]-ii)
         row.extend([0 if not attr==features[ii] else 1 
-                      for ii in range(len(features))
-                      for attr in feature_sets[ii]])
+                         for ii in range(len(features))
+                         for attr in feature_sets[ii]])
     return datas
 
-# TODO: scale the data in my highest*1.5 method
+def scale_data(datas):
+    """
+        For every row in datas we replace the value stored there with 
+        value/(max*1.5), where max is the maximum value
+        the index takes on in datas.  This modifies datas.
+    """
+    maxima = [max([ele for ele in row]) for row in datas]
+    #print 'Found in columns the following maxima.\n', maxima 
+    print 'These columns will be replaced with value/(max*1.5).'
+    for row in datas:
+        for ii in range(len(row)):
+            row[ii] = (row[ii]/(maxima[ii]*1.5))
+    return new_data 
+
+def join_data(d_ferry, d_weather):
+    """
+        This method takes in ferry data and joins appropriate weather data.
+        That is, we first match on dates (drop the data if no matching dates
+        exist) then by closest time.  Thus, we create rows of all departure 
+        data followed by all the weather data, without any dates.
+    """
+    new_data = []
+    weather_map = {}
+    # We need a nice way to look up the dates
+    for row in d_weather:
+        if row[0] in weather_map:
+            weather_map[row[0]].append(row[1:])
+        else:
+            weather_map.setdefault(row[0], [row[1:]])
+    print weather_map
+    dropped_data = 0
+    for row in d_ferry:
+        # FIXME: This comparison looks very broken since it never works
+        if row[2] in weather_map:
+            # Now we find the closest time and append weather data to ferry data
+            # in the new_data.
+            wdata = weather_map[row[0]]
+            minidxval = (0,999999999)
+            for ii in range(len(wdata)):
+                if abs(wdata[ii][0]-row[0]) < minidxval[1]:
+                    # We have a new minimum!
+                    minidxval = (ii, wdata[ii][0]-row[0])
+            join = row[:2]
+            join.extend(row[3:])
+            join.extend(wdata[ii])
+            new_data.append(join)
+        else:
+            dropped_data += 1
+    print 'We found that ', dropped_data, ' points lacked a matching weather date.'
+    print 'These points have been removed.\n'
+    return new_data
+
 # TODO: make sure to output anything done specially in the above (e.g. I have
 #   x ferries and each one corresponds to a '1' in position y of the final 
 #   feature vector)
@@ -36,16 +95,48 @@ def make_features(indices, datas):
 #   <classification label> <index1>:<value1> <index2>:<value2>. . . .\n
 #   . . . .
 
-
+#####
+# READ IN
+#####
 print 'Usage: python2 libsvm_data_format.py <weather data> <departures> <arrivals>'
 weather_read = csv.reader(open(sys.argv[1], 'rb'))
 departure_read = csv.reader(open(sys.argv[2], 'rb'))
 weather_list = [row for row in weather_read]
 departure_list = [row for row in departure_read]
+# arrival_read = csv.reader(open(sys.argv[3], 'rb'))
 print 'Weather is ', weather_list.pop(0)
 print 'Departure list is ', departure_list.pop(0)
+
+#####
+# CATEGORIZE
+#####
 # I have to remove the date from the end for departures and date from start for 
 # weather
-make_features(categoricals(departure_list[0])[:-1], departure_list)
-print departure_list[0]
-# arrival_read = csv.reader(open(sys.argv[3], 'rb'))
+categoricals_d = categoricals(departure_list[0])[:-1]
+make_features(categoricals_d, departure_list)
+print 'From make_features on departures\n', departure_list[0]
+
+categoricals_w = categoricals(weather_list[0])[1:]
+make_features(categoricals_w, weather_list)
+print 'From make_features on weather \n', weather_list[0]
+
+print '\n'
+#####
+# COMBINE WEATHER AND FERRIES
+#####
+joined_dep = join_data(departure_list, weather_list)
+print 'From join_data on departures and weather\n', joined_dep[0]
+
+
+
+#####
+# SCALE
+#####
+#scale_data(departure_list)
+#print departure_list[0]
+#all([ii <= 1 or -1 <= ii for ii in row for row in departure_list])
+
+#####
+# OUTPUT
+#####
+# Append and write
--- source/weather_data_parse.py
+++ source/weather_data_parse.py
@@ -5,7 +5,7 @@ import time
 """
     Takes weather data from the state as input and makes a huge mess of a csv 
     table containing the 
-    1 (Date), 2 (time), 4 (sky condition), 6 (visibility),
+    1 (Date), 2 (time), 6 (visibility),
     10 (dry bulb fahrenheit), 14 (wet bulb fahrenheit), 18 (dew point 
     fahrenheit), 22 (relative humidity), 24 (wind speed), 26 (wind
     direction), 30 (station pressure), 43 (altimeter)
@@ -14,7 +14,7 @@ import time
 
 data_dir = '../Data/'
 w_file = open(data_dir+'weather.csv', 'w')
-w_file.write('date, time, sky_condition, visibility, d-bulb_F, w-bulb_F, dew_point_F, rel_humidity, wind_speed, wind_dir, station_pressure, altimeter\n')
+w_file.write('date, time, visibility, d-bulb_F, w-bulb_F, dew_point_F, rel_humidity, wind_speed, wind_dir, station_pressure, altimeter\n')
 for yy in ('2010','2011','2012'):
     for mm in ('01','02','03','04','05','06','07','08','09','10','11','12'):
         if not (yy=='2011' or yy=='2010' and mm in ('12', '11', '10', '09')
@@ -32,8 +32,9 @@ for yy in ('2010','2011','2012'):
             for row in reader:
                 buildStr.append(str(datetime.strptime(row[1].strip(), "%Y%m%d").date()))
                 buildStr.append(',')
-                buildStr.append(str(time.mktime(time.strptime('1970-01-01 '+row[2].strip(), '%Y-%m-%d %H%M'))-28800))
-                for ii in (4,6,10,14,18,22,24,26,30,42):
+                tt = row[2].strip()
+                buildStr.append(str(time.mktime(time.strptime('1970-01-01 '+tt[:2]+':'+tt[2:], '%Y-%m-%d %H:%M'))-28800))
+                for ii in (6,10,14,18,22,24,26,30,42):
                    if not row[ii] in (' ', '', '  '):
                             # we don't want to insert empty data
                             buildStr.append(', ')
